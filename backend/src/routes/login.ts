0cccad8cea905df2935a14a1a4c491030de6a458
Andreirl032 <andrei.ramos.lopes@gmail.com>
2026-07-31 14:10:15 -0300

    criada rota logout,movida função authenticateToken

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -135,20 +135,20 @@ router.post("/refreshToken", async (req, res) => {
   }
 });
 
-router.get("/teste", authenticateToken, (req, res) => {
-  res.json({ oi: "ta funcionando" });
+router.post("/logout", async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    await prisma.session.delete({
+      where: {
+        token: refreshToken,
+      },
+    });
+    res.clearCookie("refreshToken");
+    return res.status(200).json({ message: "Logout realizado com sucesso" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Erro interno no servidor" });
+  }
 });
-
-function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-  //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYW5kcmVpIiwicGFzc3dvcmQiOiIxMjM0NTYiLCJpYXQiOjE3ODUzNDc1MjJ9.ejhUP5He01MbNaQPvVxj8f19RhVnyhVZCdfUO22Ok8E"
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
 
 export default router;
